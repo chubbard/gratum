5c3885114340668e1f04588f54aeafbae85dd15f
charlie <charlie@fuseanalytics.com>
2021-04-27 23:30:50 -0400

    Added a test for dates which isn't really complete.  I'd like to preparse things into dates from excel if we could know the data types before we parse.  The way we are parsing right now assumes everything is a string and is using a lot of classes from POI which makes these assumptions for us.  To do it another way and scale for really large sheets we'd have to modify those classes to create our own.

--- src/test/groovy/gratum/source/XlsxSourceTest.groovy
+++ src/test/groovy/gratum/source/XlsxSourceTest.groovy
@@ -10,7 +10,7 @@ class XlsxSourceTest {
         int id = 1
         LoadStatistic stat = XlsxSource.xlsx( "Players", Class.getResourceAsStream("/players.xlsx") ).into()
             .addStep("Verify") { Map row ->
-                assert row.size() == 5 // should have 5 columns
+                assert row.size() == 6 // should have 6 columns
                 row.each { String col, Object value ->
                     assert value != null
                 }
@@ -40,4 +40,19 @@ class XlsxSourceTest {
         assert stat.loaded == 1
         assert stat.rejections == 0
     }
+
+    @Test
+    void testXlsxDates() {
+        LoadStatistic stat = XlsxSource.xlsx("Players", Class.getResourceAsStream("/players.xlsx")).into()
+            .asDate("birth_date", "MM/dd/yyyy")
+            .addStep("Verify Dates") { Map<String,Object> row ->
+                assert row.birth_date instanceof Date
+                return row
+            }
+            .go()
+
+        assert stat.loaded == 6
+        assert stat.rejections == 0
+    }
+
 }
