e9d33308425b22c89280c91e3fbb0a79b1a88c22
charlie <charlie@fuseanalytics.com>
2021-06-07 15:11:47 -0400

    Added more Javadoc to explain why FileOpenable exists.  Also added withInputStream and withOutputStream for convience methods.

--- src/main/groovy/gratum/etl/FileOpenable.groovy
+++ src/main/groovy/gratum/etl/FileOpenable.groovy
@@ -1,6 +1,25 @@
 package gratum.etl
 
-class FileOpenable implements Openable, AutoCloseable {
+/**
+ * Represents a file that can be cast to an InputStream or OutputStream giving
+ * it the ability to masquerade as an InputStream/OutputStream.  You can only
+ * have one open at a time (ie either InputStream or OutputStream).
+ *
+ * The usage is something like this:
+ *
+ * <code>
+ *     InputStream is = row.fileOpenable as InputStream
+ *     ...
+ * </code>
+ *
+ * It understands how to convert through as keyword to java.io.InputStream,
+ * java.io.OutputStream, and java.io.File.
+ *
+ * This is mostly used by gratum to transport stream data as a file so it can
+ * be read as a stream transparently through multiple steps on the Pipline without
+ * concern for needing to centrally decorate or chain streams.
+ */
+class FileOpenable implements Openable, Closeable {
 
     private final File file
     private InputStream inStream
@@ -24,14 +43,26 @@ class FileOpenable implements Openable, AutoCloseable {
                 outStream = file.newOutputStream()
             }
             return outStream
+        } else if( clazz == File ) {
+            return file
         } else {
             throw new ClassCastException("Cannot convert to " + clazz.getName() )
         }
     }
 
     @Override
-    void close() throws Exception {
+    void close() throws IOException {
         outStream?.close()
         inStream?.close()
+    }
+
+    public <V> V withInputStream(Closure<V> closure) {
+        if( outStream ) throw new IllegalStateException("Cannot be opened as InputStream because an OutputStream already exists!")
+        return (V)file.withInputStream( closure )
+    }
+
+    public <V> V withOutputStream(Closure<V> closure) {
+        if( inStream ) throw new IllegalStateException("Cannot be opened as OutputStream because an InputStream already exists!")
+        return (V)file.withOutputStream( closure )
     }
 }
