b982dc8c68ad6e616e2ddef352fd0c23cee7052a
Charlie Hubbard <charlie.hubbard@gmail.com>
2022-06-02 17:05:56 -0400

    Allow name override (#7)
    * Added name() method to override the name of the Pipeline easily.
    
    * Added test for name override.
    
    * Upgrade from 1.0.4  -> 1.0.5
    
    * Added the apply method to call a closure with the this Pipeline as a parameter.  This allows the closure to configure many steps or other calls on the Pipeline without having to break the flow of the chained function definitions.
    
    Co-authored-by: charlie <charlie@fuseanalytics.com>

--- src/main/groovy/gratum/source/AbstractSource.groovy
+++ src/main/groovy/gratum/source/AbstractSource.groovy
@@ -6,10 +6,24 @@ abstract class AbstractSource implements Source {
 
     String name
 
+    /**
+     * This converts the source into a Pipeline to attach steps to.
+     * @return The Pipeline connected to this Source.
+     */
     @Override
     Pipeline into() {
         Pipeline pipeline = new Pipeline( name )
         pipeline.src = this
         return pipeline
+    }
+
+    /**
+     * Overrides the name of the {@see gratum.etl.Pipeline}.
+     * @param name A string with the name of the pipeline to use.
+     * @return this Source
+     */
+    public <T extends AbstractSource> T name(String name) {
+        this.name = name
+        return (T)this
     }
 }
