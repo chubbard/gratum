51bc0d8687c528affbbb84c605a6fd8321ad6688
charlie <charlie@fuseanalytics.com>
2023-11-30 11:23:07 -0500

    First pass at adding batch processing to the Sink with a test.

--- aws-sqs/src/main/groovy/gratum/aws/sink/SqsSink.groovy
+++ aws-sqs/src/main/groovy/gratum/aws/sink/SqsSink.groovy
@@ -2,6 +2,10 @@ package gratum.aws.sink
 
 import com.amazonaws.services.sqs.AmazonSQS
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder
+import com.amazonaws.services.sqs.model.SendMessageBatchRequest
+import com.amazonaws.services.sqs.model.SendMessageBatchRequestEntry
+import com.amazonaws.services.sqs.model.SendMessageBatchResult
+import com.amazonaws.services.sqs.model.SendMessageRequest
 import com.amazonaws.services.sqs.model.SendMessageResult
 import gratum.etl.Pipeline
 import gratum.sink.*
@@ -9,8 +13,10 @@ import groovy.json.JsonOutput
 
 class SqsSink implements Sink {
 
-    String queueUrl
-    int count = 0
+    private final String queueUrl
+    private int count = 0
+    private int batchSize = 1
+    private SendMessageBatchRequest batchRequest
 
     private final AmazonSQS client
 
@@ -23,6 +29,11 @@ class SqsSink implements Sink {
         return new SqsSink(queueUrl)
     }
 
+    public SqsSink batchSize(int size) {
+        this.batchSize = size
+        return this
+    }
+
     @Override
     String getName() {
         return queueUrl;
@@ -32,7 +43,22 @@ class SqsSink implements Sink {
     void attach(Pipeline pipeline) {
         pipeline.addStep("Post to SQS ${queueUrl}") { row ->
             String json = JsonOutput.toJson( row )
-            SendMessageResult result = client.sendMessage(queueUrl, json )
+            if( batchSize > 1 ) {
+                if( !batchRequest ) {
+                    batchRequest = new SendMessageBatchRequest(queueUrl)
+                }
+                if( batchRequest.getEntries().size() < batchSize ) {
+                    batchRequest.getEntries().add( new SendMessageBatchRequestEntry(
+                            UUID.newInstance().toString(),
+                            json
+                    ))
+                } else {
+                    SendMessageBatchResult res = client.sendMessageBatch( batchRequest )
+                    batchRequest = null
+                }
+            } else {
+                SendMessageResult result = client.sendMessage(queueUrl, json )
+            }
             count++
             row
         }
@@ -45,7 +71,12 @@ class SqsSink implements Sink {
 
     @Override
     void close() throws IOException {
-        client.sendMessage(queueUrl, '{"_done":true}')
+        if( batchRequest?.entries ) {
+            client.sendMessageBatch( batchRequest )
+        }
+        SendMessageRequest req = new SendMessageRequest(queueUrl, '{"_done":true}')
+            .withDelaySeconds(30)
+        client.sendMessage(req)
         client.shutdown()
     }
 }
--- aws-sqs/src/test/groovy/gratum/aws/SqsTest.groovy
+++ aws-sqs/src/test/groovy/gratum/aws/SqsTest.groovy
@@ -2,13 +2,16 @@ package gratum.aws
 
 import com.amazonaws.services.sqs.AmazonSQS
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder
+import com.amazonaws.services.sqs.model.CreateQueueRequest
 import com.amazonaws.services.sqs.model.CreateQueueResult
 import gratum.aws.sink.SqsSink
 import gratum.aws.source.SqsSource
 import gratum.etl.LoadStatistic
 import gratum.source.CollectionSource
 import org.junit.After
+import org.junit.AfterClass
 import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Ignore
 import org.junit.Test
 
@@ -17,23 +20,26 @@ import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
 
-@Ignore("Requires AWS SDK credentials setup")
+//@Ignore("Requires AWS SDK credentials setup")
 class SqsTest {
 
-    public static final String SQS_TEST_QUEUE = "SqsTest_testSqs"
+    public static final String SQS_TEST_QUEUE = "SqsTest_testSqs_${System.currentTimeMillis()}"
 
-    AmazonSQS sqs
-    CreateQueueResult queueResult
-    ExecutorService service = Executors.newFixedThreadPool(2)
+    static AmazonSQS sqs
+    static CreateQueueResult queueResult
+    static ExecutorService service
 
-    @Before
-    void setup() {
+    @BeforeClass
+    static void setup() {
+        service = Executors.newFixedThreadPool(2)
         sqs = AmazonSQSClientBuilder.defaultClient()
-        queueResult = sqs.createQueue(SQS_TEST_QUEUE)
+        CreateQueueRequest req = new CreateQueueRequest(SQS_TEST_QUEUE)
+//                .withAttributes(FifoQueue:"true")
+        queueResult = sqs.createQueue(req)
     }
 
-    @After
-    void tearDown() {
+    @AfterClass
+    static void tearDown() {
         sqs.deleteQueue(SQS_TEST_QUEUE)
         sqs.shutdown()
         service.shutdown()
@@ -73,6 +79,38 @@ class SqsTest {
         LoadStatistic statSink = sink.get()
         LoadStatistic sourceSink = source.get()
 
+        assert statSink.rejections == 0
+        assert sourceSink.rejections == 0
+        assert statSink.loaded == sourceSink.loaded
+        println(statSink)
+        println(sourceSink)
+    }
+
+    @Test
+    public void testBatchedSqs() {
+        Future<LoadStatistic> sink = service.submit({
+            List messages = (1.50).collect { id -> [ id: id, name: "Name-${id}"] }
+            CollectionSource.from(messages).save(SqsSink.queue(queueResult.queueUrl).batchSize(10))
+                    .addStep("Test that we send 50 rows") { row ->
+                        assert row["queue"] == queueResult.queueUrl
+                        assert row["count"] == 50
+                        row
+                    }
+                    .go()
+        } as Callable<LoadStatistic>)
+
+        Future<LoadStatistic> source = service.submit({
+            SqsSource.queue(queueResult.queueUrl).into()
+                    .addStep("Test we got things we expect") { row ->
+                        assert row["id"]
+                        assert row["name"]
+                        row
+                    }
+                    .go()
+        } as Callable<LoadStatistic>)
+
+        LoadStatistic statSink = sink.get()
+        LoadStatistic sourceSink = source.get()
 
         assert statSink.rejections == 0
         assert sourceSink.rejections == 0
