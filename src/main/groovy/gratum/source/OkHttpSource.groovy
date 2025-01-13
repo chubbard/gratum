187e254d8c10b4d38c611fc96a92af060b40840f
charlie <charlie@fuseanalytics.com>
2025-01-13 18:49:52 -0500

    Added javadoc, and helper methods for sharing the OkHttpClient per best practices. Also added retry logic when receiving a 429 http code with a Retry-After header.

--- src/main/groovy/gratum/source/OkHttpSource.groovy
+++ src/main/groovy/gratum/source/OkHttpSource.groovy
@@ -21,10 +21,18 @@ import okhttp3.Request
  *  .printRow()
  *  .go
  * </pre>
- *
- * For URLs that return "application/json" or either "text/xml" or "application/xml" the source will
- * process the body using JsonSlurper or XmlSlurper and return properties for
- * json and xml, respectively.
+ * <p>
+ * The row played down the Pipeline when using this source will consist of the following columns:
+ *</p>
+ * <p>
+ * <ul>
+ * <li>response - a OkHttp response object.</li>
+ * <li>body - The OkHttp Body object of the response (ie response.body())</li>
+ * <li>status - The HTTP status code returned (ie response.code())</li>
+ * <li>json - A parsed json object returned from JsonSlurper if the Content-Type returned is "application/json"</li>
+ * <li>xml - A parse xml object returned from XmlSlurper if the Content-Type returned is "text/xml" or "application/xml"</li>
+ * </ul>
+ * </p>
  */
 @CompileStatic
 class OkHttpSource extends AbstractSource {
@@ -68,31 +76,109 @@ class OkHttpSource extends AbstractSource {
             return this
         }
 
+        /**
+         * Helper method to add Bearer authentication header to the request.
+         *
+         * @param jwtToken the token to add to the header
+         * @return this instance
+         */
         OkHttpBuilder authToken(String jwtToken) {
             builder.header("Authorization", "Bearer ${jwtToken}")
             return this
         }
 
-        OkHttpBuilder query(Map params) {
+        /**
+         * Closure to configure the client for this OkHttpSource.  This allows you to override the default client
+         * configuration for this use of OkHttpSource.
+         */
+        OkHttpBuilder client(@DelegatesTo(OkHttpClient.Builder) Closure configure) {
+            client = client.newBuilder().with {
+                configure.delegate = it
+                configure()
+                return it
+            }.build()
+            return this
+        }
+
+        /**
+         * Helper method to add query parameters to the existing url.
+         * @param params A Map containing the parameter names and values
+         * @return this instance
+         */
+        OkHttpBuilder query(Map<String,Object> params) {
             HttpUrl.Builder urlBuilder = url.newBuilder()
-            params.each { x,y -> urlBuilder.addQueryParameter(x as String,y as String) }
+            params.each { x,y -> urlBuilder.addQueryParameter(x,y as String) }
             url = urlBuilder.build()
             return this
         }
     }
 
-    OkHttpSource(String url, Closure configure) {
+    /**
+     * The constructor of OkHttpSource used internally.  Use one of the static helper methods when constructing an
+     * instance of this type.
+     * @param url
+     * @param client
+     * @param configure
+     */
+    OkHttpSource(String url, OkHttpClient client, Closure configure) {
         super(url)
         this.url = HttpUrl.parse(url)
         this.configure = configure
+        this.client = client
     }
 
+    /**
+     * This creates an OkHttpSource with its own OkHttpClient which won't be shared.  This is only useful
+     * when using only 1 OkHttpSource.  If you have multiple OkHttpSources within your pipeline or you execute
+     * multiple calls consider shared the OkHttpClient instance between them using:
+     *
+     * {@link OkHttpSource#http(java.lang.String, okhttp3.OkHttpClient, groovy.lang.Closure)}
+     *
+     * @param url the url to load
+     * @param configure a closure to configure the request or other aspects of this call.
+     * @return The OkHttpSource
+     */
     static OkHttpSource http( String url, @DelegatesTo(OkHttpBuilder) Closure configure = null ) {
-        return new OkHttpSource( url, configure )
+        return http( url, new OkHttpClient(), configure )
     }
 
+    /**
+     * Best practice is to share OkHttpClients when performing multiple http calls or multiple OkHttpSources.  This
+     * enables sharing the OkHttpClient across multiple OkHttpSource instances.
+     * @param url the url to load
+     * @param client the shared OkHttpClient instance
+     * @param configure a closure to configure the request or other aspects of this call.
+     * @return The OkHttpSource
+     */
+    static OkHttpSource http(String url, OkHttpClient client, @DelegatesTo(OkHttpBuilder) Closure configure = null ) {
+        return new OkHttpSource(url, client, configure)
+    }
+
+    /**
+     * This creates an OkHttpSource with its own OkHttpClient which won't be shared.  This is only useful
+     * when using only 1 OkHttpSource.  If you have multiple OkHttpSources within your pipeline or you execute
+     * multiple calls consider shared the OkHttpClient instance between them using:
+     *
+     * {@link OkHttpSource#https(java.lang.String, okhttp3.OkHttpClient, groovy.lang.Closure)}
+     *
+     * @param url the url to load
+     * @param configure a closure to configure the request or other aspects of this call.
+     * @return The OkHttpSource
+     */
     static OkHttpSource https( String url, @DelegatesTo(OkHttpBuilder) Closure configure = null ) {
-        return new OkHttpSource( url, configure )
+        return https( url, new OkHttpClient(), configure )
+    }
+
+    /**
+     * Best practice is to share OkHttpClients when performing multiple http calls or multiple OkHttpSources.  This
+     * enables sharing the OkHttpClient across multiple OkHttpSource instances.
+     * @param url the url to load
+     * @param client the shared OkHttpClient instance
+     * @param configure a closure to configure the request or other aspects of this call.
+     * @return The OkHttpSource
+     */
+    static OkHttpSource https(String url, OkHttpClient client, @DelegatesTo(OkHttpBuilder) Closure configure = null) {
+        return new OkHttpSource( url, client, configure )
     }
 
     @Override
@@ -102,21 +188,31 @@ class OkHttpSource extends AbstractSource {
             configure.delegate = wrapper
             configure()
         }
-        wrapper.client.newCall(wrapper.build()).execute().withCloseable { response ->
-            Map<String,Object> result = [response: response, body: response.body(), status: response.code()]
-            String contentType = response.header("Content-Type")?.split(";")?.first()
-            switch(contentType) {
-                case "application/json":
-                    JsonSlurper json = new JsonSlurper()
-                    result.json = json.parse( response.body().charStream() )
-                    break
-                case "application/xml":
-                case "text/xml":
-                    XmlSlurper xml = new XmlSlurper()
-                    result.xml = xml.parse( response.body().charStream() )
-                    break
+        boolean done = false
+        while( !done ) {
+            wrapper.client.newCall(wrapper.build()).execute().withCloseable { response ->
+                if (response.code() == 429) {
+                    Integer retryAfter = response.header("Retry-After") as Integer
+                    Thread.sleep(retryAfter * 1000L)
+                    done = false
+                } else {
+                    Map<String, Object> result = [response: response, body: response.body(), status: response.code()]
+                    String contentType = response.header("Content-Type")?.split(";")?.first()
+                    switch (contentType) {
+                        case "application/json":
+                            JsonSlurper json = new JsonSlurper()
+                            result.json = json.parse(response.body().charStream())
+                            break
+                        case "application/xml":
+                        case "text/xml":
+                            XmlSlurper xml = new XmlSlurper()
+                            result.xml = xml.parse(response.body().charStream())
+                            break
+                    }
+                    pipeline.process(result, 1)
+                    done = true
+                }
             }
-            pipeline.process(result,1)
         }
     }
 }
